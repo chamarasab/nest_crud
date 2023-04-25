34bbec07e1eaf24113c8370729c416dc4c161d76
chamarasab <chamara.sab@outlook.com>
2023-04-25 15:24:00 +0530

    created new module named users ✔

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { MongooseModule } from '@nestjs/mongoose';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot(
       'mongodb+srv://chamaraorbit:AEzakmi6526560@crud.yrmq2zr.mongodb.net/crud_db?retryWrites=true&w=majority',
     ),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
