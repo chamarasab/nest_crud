1326611d22206e005d5e55f92ffc3389488f0bb8
chamarasab <chamara.sab@outlook.com>
2023-04-25 15:18:51 +0530

    setup mongodb database connection ✔

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://chamaraorbit:AEzakmi6526560@crud.yrmq2zr.mongodb.net/crud_db?retryWrites=true&w=majority',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
