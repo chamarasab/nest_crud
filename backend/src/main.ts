aed0a41466442f164fcfa41ea22bdc87660bcf69
chamarasab <chamara.sab@outlook.com>
2023-04-26 12:31:45 +0530

    data retrieve completed  ✔

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,11 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+//import { CorsMiddleware } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  // Enable CORS
+  //app.use(CorsMiddleware);
   await app.listen(3000);
 }
 bootstrap();
