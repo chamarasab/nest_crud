103c0411b8c13c67a5ca72300adb0f051bee5c00
chamarasab <chamara.sab@outlook.com>
2023-04-26 10:28:29 +0530

    set nestjs api to get data from angular ✔

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {NgOptimizedImage} from "@angular/common";
 import { AllUsersComponent } from './users/all-users/all-users.component';
-import {HttpClient, HttpClientModule} from "@angular/common/http";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
--- frontend/src/app/users/users.service.ts
+++ frontend/src/app/users/users.service.ts
@@ -1,6 +1,5 @@
 import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import * as http from "http";
 import {Users} from "./users";
 
 @Injectable({
