861021e0263a847e3af7ad5b4b90e3a8e6551a5e
chamarasab <chamara.sab@outlook.com>
2023-04-26 09:37:34 +0530

    calling users/all-users component ✔

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
+// @ts-ignore
+
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {AllUsersComponent} from "./users/all-users/all-users.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component:AllUsersComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
