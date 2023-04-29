1c552bf38d2ac78ffdfe69666489dc2788f99b92
chamarasab <chamara.sab@outlook.com>
2023-04-29 09:35:08 +0530

    Add createUsers method to UsersController ✔

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,11 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { Users } from './schema/users.schema';
 
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
+
   @Get()
   async getAll() {
     return this.usersService.getAll();
+  }
+
+  @Post()
+  async createUsers(@Body() users: Users) {
+    return this.usersService.create(users);
   }
 }
